9894578dec709b124c83e48f03c0ad75ca9e18df
WildPastry <mparkernzdev@gmail.com>
2023-11-21 18:13:17 +1300

    Update backend

--- server/routes/record.js
+++ server/routes/record.js
@@ -1,17 +1,17 @@
 const express = require("express");
 
-// recordRoutes is an instance of the express router.
-// We use it to define our routes.
-// The router will be added as a middleware and will take control of requests starting with path /record.
+// recordRoutes is an instance of the express router
+// We use it to define our routes
+// The router will be added as a middleware and will take control of requests starting with path /record
 const recordRoutes = express.Router();
 
 // This will help us connect to the database
 const dbo = require("../db/conn");
 
-// This help convert the id from string to ObjectId for the _id.
+// This help convert the id from string to ObjectId for the _id
 const ObjectId = require("mongodb").ObjectId;
 
-// This section will help you get a list of all the records.
+// This section will help you get a list of all the records
 recordRoutes.route("/record").get(function (req, res) {
   let db_connect = dbo.getDb("employees");
   db_connect
@@ -33,7 +33,7 @@ recordRoutes.route("/record/:id").get(function (req, res) {
   });
 });
 
-// This section will help you create a new record.
+// This section will help you create a new record
 recordRoutes.route("/record/add").post(function (req, response) {
   let db_connect = dbo.getDb();
   let myobj = {
@@ -47,7 +47,7 @@ recordRoutes.route("/record/add").post(function (req, response) {
   });
 });
 
-// This section will help you update a record by id.
+// This section will help you update a record by id
 recordRoutes.route("/update/:id").post(function (req, response) {
   let db_connect = dbo.getDb();
   let myquery = { _id: ObjectId(req.params.id) };
